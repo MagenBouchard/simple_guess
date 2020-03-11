0c3a717fd38879e51de9d63d731ae1b93956b16c
Magen Bouchard <Magen.Bouchard@gmail.com>
2020-03-11 15:52:46 -0400

    added comments

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # simple_guess
 
 
-## This simple guess game uses JavaScript, HTML, and CSS.
+## This simple guess game uses 
+JavaScript, HTML, and CSS.
 
-
+## 
 This game first determines the random number by using Math.random(). The player has ten attempts to guess the correct number. 
 
-Each guess logic checks the number the user inputs agaisnt the random number previously determined. Each check provides feedback to the user. The user input and the computer's number is compated and if the user's number is too high text is dynnamically added to the page to tell the user the number is too high. If the number less then that feedback is provided dynamically as well.
+Each guess logic checks the number the user has entered agaisnt the random number previously determined. Each check provides feedback to the user. The user input and the computer's number is compared and if the user's number is too high text is dynnamically added to the page to tell the user the number is too high. If the number is less than, then feedback is provided and added dynamically to the page as well.
 
-If the user guess and the previously determined number is equal then the text dynamically added to the page lets the user know. 
+If the user guess and the previously determined number is equal and less than ten attempts has occured then the text dynamically added to the page lets the user know.
--- main.js
+++ main.js
@@ -1,48 +1,42 @@
+//variables are set 
 var computerGuess;
-var userGuessLog = [];  
-var attempts = 0; 
-var maxAttempts =10;
+var userGuessLog = [];
+var attempts = 0;
+var maxAttempts = 10;
 
 function maxGuess() {
-    maxGuess = 10; 
+  maxGuess = 10;
 }
 
-function newGame () {
-    window.location.reload(); 
+function newGame() {
+  window.location.reload();
 }
-
 
 function init() {
- computerGuess = Math.floor(Math.random() * 100 + 1);
- console.log(computerGuess);
+  computerGuess = Math.floor(Math.random() * 100 + 1);
+  console.log(computerGuess);
 }
-
-
-
-
 
 function compareGuess() {
+  var userGuess = document.getElementById("inputBox").value;
+  // console.log(userGuess);
 
- var userGuess = document.getElementById("inputBox").value;
- // console.log(userGuess);
+  userGuessLog.push(userGuess);
+  // console.log(userGuessLog);
+  
+  // increments the number of attempts by the user
+  attempts++;
 
+  //displays the number of attempts to the page
+  document.getElementById("attempts").innerHTML = attempts;
 
-userGuessLog.push(userGuess);
-// console.log(userGuessLog);
-
-attempts++; 
-document.getElementById("attempts").innerHTML = attempts;
-
-
-
-if (userGuess > computerGuess) {
- document.getElementById("textOut").innerHTML = "Too high of a number!";
- document.getElementById('inputBox').value="";
-} else if (userGuess < computerGuess) {
- document.getElementById("textOut").innerHTML = "The number is too low!";
- document.getElementById('inputBox').value="";
-} else { 
- document.getElementById("textOut").innerHTML = "You are Correct!"; 
+  if (userGuess > computerGuess) {
+    document.getElementById("textOut").innerHTML = "Too high of a number!";
+    document.getElementById("inputBox").value = "";
+  } else if (userGuess < computerGuess) {
+    document.getElementById("textOut").innerHTML = "The number is too low!";
+    document.getElementById("inputBox").value = "";
+  } else {
+    document.getElementById("textOut").innerHTML = "You are Correct!";
+  }
 }
-}
-
